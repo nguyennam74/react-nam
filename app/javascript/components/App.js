f629d42dc6a849195f21a5781b0ae81d99d67d76
namnv1 <namnv1@nal.vn>
2018-11-11 22:54:56 +0700

    life cycle

---
+++ app/javascript/components/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import NoiDung from "./NoiDung";
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            trangthai: "khoitao",
+            trangthai2: "khoitao2"
+        }
+    }
+
+    componentWillMount() {
+        console.log("componentWillMOunt chay");
+    }
+
+    componentDidMount(){
+        console.log("componentDidMount da chay");
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log("shouldComponentUpdate da chay");
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log("componentWillUpdate da chay");
+    }
+
+    componentDidUpdate(nextProps, nextState) {
+        console.log("componentDidUpdate da chay");
+    }
+
+    capNhatState = () => {
+        this.setState(
+            {
+                trangthai: "trang thai duoc update"
+            }
+        );
+    }
+    render(){
+        console.log('day la render da chay');
+        console.log(this.state.trangthai);
+        return (
+
+          <div className="App">
+            <NoiDung ten={this.state.trangthai2}></NoiDung>
+            <button onClick={() => this.capNhatState()}>Click update state</button>
+          </div>
+        );
+    }
+}
+export default App;
---
+++ app/javascript/components/NoiDung.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class NoiDung extends React.Component {
+    render() {
+        return (
+           <h4> {this.props.ten} </h4>
+        );
+    }
+}
+
+export default NoiDung;
